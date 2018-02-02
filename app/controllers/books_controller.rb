cd8bea1dd4e0f2b564b8b795045b53f806b9c120
Grzegorz Korzeniowski <korzeniowski.g@gmail.com>
2018-02-02 14:52:51 +0100

    Change condition in where

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.where("name LIKE '%#{params[:search]}%' AND published='t'")
+    @books = Book.where("name LIKE '%#{params[:search]}%'")
   end
 
   # GET /books/1
